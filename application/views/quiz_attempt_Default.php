62890b882a958086c05c6b1615ce28a802a528d1
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-09-10 12:51:40 +0530

    disabled keys on fullscreen code

--- application/views/quiz_attempt_Default.php
+++ application/views/quiz_attempt_Default.php
@@ -502,8 +502,7 @@ function increasectime(){
 	let ele = '<div id="popup"><div class="pane">The test window should be attempted in fullscreen.<br><br><button class="btn" onClick="javascript:requestFullScreen();">OK</button></div></div>';
 	$('body').prepend(ele);
 	function requestFullScreen() {
-		var el = document.getElementById('popup');
-		el.style.display = "none";
+		var el = document.documentElement;
 		// Supports most browsers and their versions.
 		var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
 		if (requestMethod) {
@@ -516,9 +515,8 @@ function increasectime(){
 				wscript.SendKeys("{F11}");
 			}
 		}
-		el.style.width=0;
-		el.style.height=0;
-		//el.parentNode.removeChild(el); 
+		
+		document.getElementById('popup').parentNode.removeChild(document.getElementById('popup'));
 	}
 
 	document.addEventListener("fullscreenchange", onFullScreenChange, false);
@@ -530,12 +528,37 @@ function increasectime(){
 	  // if in fullscreen mode fullscreenElement won't be null
 	  if(!fullscreenElement){
 		$('body').prepend(ele);
-		let p = document.getElementById('popup');
-	  	p.style.display = "block";
-		p.style.width = "100%";
-		p.style.height = "100%";
 	  }
 
+	}
+	
+	document.onkeydown = function(e){
+		disableKeys(e);
+	}
+
+	function disableKeys(e){
+		if (e.target) t = e.target; //Firefox and others
+		else if (e.srcElement) t = e.srcElement; //IE
+		if (e.keyCode == 116) //prevent F5 for refresh
+		    e.preventDefault();
+		if (e.keyCode == 122) //F11 leave fullscreen
+		    e.preventDefault()
+		else if (e.altKey && e.keyCode == 115) //Alt + F4
+		    e.preventDefault();
+		else if (e.altKey && e.keyCode == 37) //Alt + left
+		    e.preventDefault();
+		else if (e.altKey && e.keyCode == 39) //Alt + right
+		    e.preventDefault();
+		else if (e.ctrlKey && e.keyCode == 82) //Ctrl + R (reload)
+		    e.preventDefault();
+		if (!(t.tagName == 'INPUT')) {
+		    if (e.keyCode == 13) { //enter
+		        e.preventDefault();
+		    }
+		    if (e.keyCode == 8) { //backspace
+		        e.preventDefault();
+		    }
+		}
 	}
 
 </script>
