bc8e1181d62567f13e2fe71735b544c9e67fdef1
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-09-11 18:29:41 +0530

    .

--- application/views/quiz_attempt_Default.php
+++ application/views/quiz_attempt_Default.php
@@ -532,8 +532,8 @@ function increasectime(){
 		  console.log(warnings);
 		  if(warnings >= 3) {
 			  console.log("trigger");
-			  $('#r-submit').trigger('click');
 			  let msg = "<did id='popup'><div class='pane'>Submitting quiz...</div></div>";
+			  setTimeout(function(){ window.location = "/index.php/quiz_submit_quiz"; console.log("timeout")}, 2000);
 			  $('body').prepend(msg);
 			  return;
 		  }
